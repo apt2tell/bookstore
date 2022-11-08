78c0f274917e1678ab9dec4791d3e8da4d69d83a
Okechukwu <okei.muokwugwo@outlook.com>
2022-11-08 21:34:54 +0100

    Add action type for checking  status

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,3 +1,5 @@
+const CHECK_STATUS = 'bookstore-CMS/categories/CHECK_STATUS';
+
 const initialState = [];
 
 export const checkStatus = () => ({
@@ -6,8 +8,8 @@ export const checkStatus = () => ({
 
 const categoriesReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'CHECK_STATUS':
-      return [...state, 'IN_PROGRESS'];
+    case CHECK_STATUS:
+      return 'Under construction';
 
     default:
       return state;
