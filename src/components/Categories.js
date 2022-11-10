658748aef5d607ccf1c0628c184fb6b25265fbad
Okechukwu <okei.muokwugwo@outlook.com>
2022-11-10 09:44:20 +0100

    Dispatch checkStatus  action in the handleCheckStatus  method

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
-  <>
-    <button type="button">Check Status</button>
-  </>;
+  const dispatch = useDispatch();
+
+  const status = useSelector((state) => state.categories);
+  // handler for check status
+  const handleCheckStatus = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <>
+      <button onClick={handleCheckStatus} type="button">Check Status</button>
+      <span>{status}</span>
+    </>
+  );
 };
 
 export default Categories;
