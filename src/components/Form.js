fdd15fb074168a79f8510b8b6145be7d26b08063
Okechukwu <okei.muokwugwo@outlook.com>
2022-11-10 09:45:33 +0100

    Dispatch addBook action in the handleAddBook method

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { nanoid } from 'nanoid';
+import { addBook } from '../redux/books/books';
 
-const Form = () => (
-  <form className="form">
-    <input type="text" placeholder="Book title" />
-    <input type="text" placeholder="Author" />
-    <button type="button">ADD BOOK</button>
-  </form>
-);
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
+
+  // handle Add book
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    const book = {
+      id: nanoid(),
+      title,
+      author,
+    };
+    dispatch(addBook(book));
+    setTitle('');
+    setAuthor('');
+  };
+
+  // setTitle handler
+  const handleChangeTitle = (e) => (setTitle(e.target.value));
+
+  // setAuthor handler
+  const handleChangeAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleAddBook} className="form">
+      <input onChange={handleChangeTitle} type="text" placeholder="Book title" />
+      <input onChange={handleChangeAuthor} type="text" placeholder="Author" />
+      <button type="submit">ADD BOOK</button>
+    </form>
+  );
+};
 
 export default Form;
