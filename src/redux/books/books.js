e623f23ed6e180a4b59fcbb6a0f4489180ffd4da
Okechukwu <okei.muokwugwo@outlook.com>
2022-11-10 09:48:41 +0100

    Update initialState with array of books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,22 +1,43 @@
-const ADD_BOOK = 'bookstore-CMS/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookstore-CMS/books/REMOVE_BOOK';
+const ADD_BOOK = 'Bookstore-CMS/books/ADD_BOOK';
+const REMOVE_BOOK = 'Bookstore-CMS/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    title: 'Things Fall Apart',
+    author: 'Chinua Achebe',
+  },
+  {
+    id: 2,
+    title: 'Treasure Island',
+    author: 'Bernard Shaw',
+  },
+  {
+    id: 3,
+    title: 'Tempest',
+    author: 'Sheakspeare',
+  },
+  {
+    id: 4,
+    title: 'Merchant of Venice',
+    author: 'Sheakspeare',
+  },
+];
 
-export const addBook = (payload) => ({
+export const addBook = (book) => ({
   type: ADD_BOOK,
-  payload,
+  payload: book,
 });
 
-export const removeBook = (payload) => ({
+export const removeBook = (id) => ({
   type: REMOVE_BOOK,
-  payload,
+  payload: id,
 });
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, { title: action.payload.title, author: action.payload.author }];
+      return [...state, action.payload];
 
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
