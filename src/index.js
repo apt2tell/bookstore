3256daa777ae1785c7edff4338de6b0a878eeb82
Okechukwu <okei.muokwugwo@outlook.com>
2022-12-04 10:48:10 +0100

    Update BrowserRouter with HashBrowser Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import store from './redux/configureStore';
